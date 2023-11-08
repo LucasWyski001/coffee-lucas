4e9c54bfea0fe23fc39951390b2657fa713d7b47
Lucas Wysoczanski <109931537+LucasWyski001@users.noreply.github.com>
2023-11-08 02:41:37 -0600

    added a nav bar now need to edit

--- coffeewebsite/src/App.js
+++ coffeewebsite/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import { NavBar } from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
         {/*all the different sections of the app will come here*/}
-
+      <NavBar />
     </div>
   );
 }
