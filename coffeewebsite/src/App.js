9ecef7b45128ca61c2c81b3f053117a3bc8eb86d
Lucas Wysoczanski <109931537+LucasWyski001@users.noreply.github.com>
2023-11-15 15:57:00 -0600

    created header section as well as content section for the application

--- coffeewebsite/src/App.js
+++ coffeewebsite/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { NavBar } from './components/NavBar';
+import { Content} from './components/Content';
 
 function App() {
   return (
     <div className="App">
         {/*all the different sections of the app will come here*/}
       <NavBar />
+      <Content />
     </div>
   );
 }
