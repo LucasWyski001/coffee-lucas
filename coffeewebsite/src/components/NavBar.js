178d5caf1ff1bd8ce728dc878ae8fc8422523eef
Lucas Wysoczanski <109931537+LucasWyski001@users.noreply.github.com>
2023-11-16 03:28:57 -0600

    added more updates

--- coffeewebsite/src/components/NavBar.js
+++ coffeewebsite/src/components/NavBar.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import coffeeImg from "../assets/img/cofeeEx.jpg";
 
 const navigation = [
   { name: 'About', href: '#' },
